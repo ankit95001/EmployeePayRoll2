b0259753af4133f398d5ed60970fa724cf557b1d
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-27 15:48:05 +0530

    [Ankit] DataBase COndiguration changed

--- src/main/java/com/bridgelabz/employeepayroll/entity/Employee.java
+++ src/main/java/com/bridgelabz/employeepayroll/entity/Employee.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.employeepayroll.entity;
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,35 +12,29 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name = "employees")
 public class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name is required")
-    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]+$", message = "Name must start with a capital letter and contain only letters")
+    @Column(nullable = false)
     private String name;
 
-    @NotNull(message = "Salary is required")
-    @Min(value = 10000, message = "Salary must be at least 10,000")
+    @Column(nullable = false)
     private Double salary;
 
-    @NotBlank(message = "Gender is required")
-    @Pattern(regexp = "^(Male|Female|Other)$", message = "Gender must be Male, Female, or Other")
+    @Column(nullable = false)
     private String gender;
 
-    @NotNull(message = "Start Date is required")
-    @PastOrPresent(message = "Start Date must be today or in the past")
-    @JsonFormat(pattern="dd MMM yyyy")
+    @Column(nullable = false)
     private LocalDate startDate;
 
-    @NotBlank(message = "Note cannot be empty")
+    @Column(nullable = false)
     private String note;
 
-    @NotBlank(message = "Profile picture cannot be empty")
+    @Column(nullable = false)
     private String profilePic;
 
     @ElementCollection
-    @NotEmpty(message = "Department list cannot be empty")
     private List<String> department;
 }
-
